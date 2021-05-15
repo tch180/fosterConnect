938cd4c207de78c4c4243b97a8abd0b7e06903d1
TCH 180 <thopstadredner@gmail.com>
2021-05-15 00:52:15 -0400

    cleaned up post and added a little more styling.

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -16,17 +16,16 @@ function Post() {
         <div className="container px-4 py-5" id="hanging-icons">
           <h2 className="pb-2 border-bottom">Your Post</h2>
           <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
-            <div className="col d-flex align-items-start">
-              <div className="icon-square bg-light text-dark flex-shrink-0 me-3"></div>
-              <div>
-                {post.map((post) => (
-                  <div key={post._id}>
-                    <h2>{post.postTitle}</h2>
+            {post.map((post) => (
+              <div key={post._id} className="col d-flex align-items-start">
+                <div>
+                  <div>
+                    <h2>{post.postTitle.toUpperCase()}</h2>
                     <p>{post.postText}</p>
                   </div>
-                ))}
+                </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
