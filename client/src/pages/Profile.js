c56e59ad9e162dfc290813b31d26507a09c76973
TCH 180 <thopstadredner@gmail.com>
2021-05-14 22:40:10 -0400

    update profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,6 +2,7 @@
 import React, { useContext, useEffect } from 'react'
 import AuthContext from '../context/Auth/authContext'
 import ProfileSidebar from '../layout/ProfileSidebar'
+import UsersPost from '../components/Post/Post'
 
 
 
@@ -22,46 +23,7 @@ function Profile(props) {
     return (
         <div style={{color:'teal',display:'flex', flexDirection:'row'}}>
             <ProfileSidebar/>
-      <div >
-      <div class="container px-4 py-5" id="hanging-icons">
-    <h2 class="pb-2 border-bottom">Your Post</h2>
-    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-      <div class="col d-flex align-items-start">
-        <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
-        </div>
-        <div>
-          <h2>Featured title</h2>
-          <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-          <a href="#" class="btn btn-primary">
-            Primary button
-          </a>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
-        </div>
-        <div>
-          <h2>Featured title</h2>
-          <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-          <a href="#" class="btn btn-primary">
-            Primary button
-          </a>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
-        </div>
-        <div>
-          <h2>Featured title</h2>
-          <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-          <a href="#" class="btn btn-primary">
-            Primary button
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-      </div>
+            <UsersPost/>
       
 
         </div>
